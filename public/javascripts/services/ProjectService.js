5b653b65f07698fa072e86d94b24a37a00efd0f2
Alex Tang <tang.alex.93@gmail.com>
2015-06-16 13:37:29 -0700

    Add ProjectService.js

---
+++ public/javascripts/services/ProjectService.js
@@ -0,0 +1,28 @@
+/// <reference path="../../../typings/angularjs/angular.d.ts"/>
+/**
+ * Angular factory used for all CRUD operations for the MoProjectdel model.
+ * This module can be included into any other module as a dependency, 
+ * the factory associated with this module is used for the CRUD operations.
+ */ 
+angular.module('ProjectService', []).factory('Project', ['$http', function($http) {
+
+    return {
+        // call to get
+        get : function() {
+            return $http.get('/api/project');
+        },
+
+
+        // these will work when more API routes are defined on the Node side of things
+        // call to POST to create
+        create : function(project) {
+            return $http.post('/api/project', project);
+        },
+
+        // call to DELETE 
+        delete : function(id) {
+            return $http.delete('/api/project/' + id);
+        }
+    };       
+
+}]);
