d62db938f9d8bf8fd3d4feb3f94bba29a2fc8c6e
Alex Tang <tang.alex.93@gmail.com>
2015-08-03 17:12:37 -0700

    Comments for AdminViews

--- public/AdminViews/AdminCtrl.js
+++ public/AdminViews/AdminCtrl.js
@@ -1,5 +1,5 @@
 /**
- * Controllers for the admin pages
+ * Angularjs module for all the controllers that belong to the admin pages.
  */
 (function() {
     var app = angular.module('AdminControllers', [
@@ -7,18 +7,37 @@
         "ProjectService",
         "angularModalService"
     ]);
-
+    
+    /**
+     * =======================================================================
+     * Controller for approveView.html
+     * 
+     * approveView is the html swapped into ng-view to show the administrators
+     * all of the projects which have not been approved, it also allows them to
+     * approve of projects and set the start date for funding.
+     * ========================================================================
+     */
     app.controller("ApproveCtrl", ["$scope", "$rootScope", "Project", "User", "$routeParams", "ModalService",
         function($scope, $rootScope, Project, User, $routeParams, ModalService) {
-
+ 
+            $scope.projects;            // array, all non-approved projects. Empty if administrator is not authenticated.
+            $scope.verified = false;    // boolean, whether or not the administrator has been verified.
+            
+            /**
+             * Move the page down as soon as it is loaded
+             * so that the authentication is in view.
+             */
             $scope.$on('$viewContentLoaded', function() {
                 $.fn.fullpage.moveSectionDown();
             });
-
-
-
-            $scope.projects;
-            $scope.verified = false;
+            
+            /**
+             * Called when the administrator presses the 'show' button.
+             * Calls to the server to retrieve all of the projects that have not been approved 
+             * and assigns them to $scope.projects.
+             * It also sends along the password and verification is done at the server side.
+             * If the password is incorrect, null is returned as well as a HTTP 400 message.
+             */
             $scope.tryPassword = function() {
 
                 Project.getNonApproved($scope.password)
@@ -26,7 +45,8 @@
                         $scope.verified = true;
 
                         $scope.projects = data;
-
+                        
+                        // TODO: Refactor into it's own method.
                         $scope.projects.forEach(function(project) {
                             var days_left = (new Date(project.end_date).getTime() - new Date().getTime()) / 1000 / 60 / 60 / 24;
                             project.days_left = days_left > 0 ? days_left : 0;
@@ -42,16 +62,19 @@
                                     project.viewStarImage = "/assets/icons/card-icons/Star.svg";
                                 }
                             });
-
                         });
-
-
-
                     });
             }
-
+            
+            /**
+             * Parameter: Index 
+             *  - Index of the project clicked in $scope.projects
+             * 
+             * This method will call out to the server and set a project
+             * as approved. Once approved, the project will be deleted
+             * from this view and will show up in the non-administrator pages.
+             */
             $scope.approveProject = function(index) {
-
                 swal({
                         title: "Approve",
                         text: "When will funding start?",
@@ -62,6 +85,7 @@
                         animation: "slide-from-top"
                     },
                     function(start_date) {
+                        // start_date is the value entered prompt.
                         if (start_date === false) {
                             return false;
                         }
@@ -70,18 +94,20 @@
                         } else {
                             Project.approveProject($scope.projects[index]._id, start_date)
                                 .success(function(data) {
-
                                     swal("You have approved the project!");
+                                    // Remove the project from $scope.projects
                                     if (index > -1) {
                                         $scope.projects.splice(index, 1);
                                     }
-                                })
+                                });
                         }
                     })
-
             };
 
-
+            /**
+             * Given the name of the category, return the corresponding color
+             * that is associated with it.
+             */
             $scope.selectColorByCategory = function(category_name) {
                 var name_lower = String(category_name).toLowerCase();
                 switch (name_lower) {
@@ -97,7 +123,10 @@
                         return "smokewhite";
                 }
             }
-
+            
+            /**
+             * Display a modal that allows the administrator to preview the project.
+             */
             $scope.openProject = function(projectId, index) {
                 ModalService.showModal({
                     templateUrl: 'ProjectView/projectView.html',
@@ -109,9 +138,7 @@
                         Image: null
                     }
                 }).then(function(modal) {
-                    modal.element.modal({
-                        //backdrop: 'static'
-                    });
+                    modal.element.modal({});
                     modal.close.then(function(result) {
 
                     });
@@ -119,36 +146,67 @@
             };
 
         }
-    ]);
+    ]);  // ApproveCtrl END.
 
 
+
+    /**
+     * =======================================================================
+     * Controller for usersView.html
+     * 
+     * usersView is the html swapped into ng-view to show the administrators
+     * all of the users that have registered with the system. The registered users
+     * are any users that have visited the website.
+     * 
+     * users can be either budget owners or not. This view allows the adminstrators
+     * to set a user as a budget owner which will allow them to start funding projects.
+     * ========================================================================
+     */
     app.controller("UsersViewCtrl", ["$scope", "Project", "User", "$routeParams",
         function($scope, Project, User, $routeParams) {
+            
+            $scope.verified;    // boolean, if the administrator has authenticated with the correct password.
+            $scope.users;       // array, all the users registered in the system. Empty if not verified.
+            $scope.query;       // string, search query for users
+            
+            /**
+             * Move the page down as soon as it is loaded
+             * so that the authentication is in view.
+             */
             $scope.$on('$viewContentLoaded', function() {
                 $.fn.fullpage.moveSectionDown();
             });
-
-
-            $scope.verified = false;
-            $scope.users;
+    
+            /**
+             * Called when the administrator presses the 'show' button.
+             * Calls to the server to retrieve all of the users that are registered in the system 
+             * and assigns them to $scope.users.
+             * 
+             * It also sends along the password and verification is done at the server side.
+             * If the password is incorrect, null is returned as well as a HTTP 400 message.
+             */
             $scope.tryPassword = function() {
-
-                User.getAllUsers($scope.password)
-                    .success(function(data) {
-                        $scope.verified = true;
-                        $scope.users = data;
-                    });
+                    User.getAllUsers($scope.password)
+                        .success(function(data) {
+                            $scope.verified = true;
+                            $scope.users = data;
+                        });
             }
-
+            
+            /**
+             * Parameter: index
+             *  - index of the user to be made into a budget owner in $scope.users
+             * 
+             * Call to the server and set the user to be a budget owner if they are not already.
+             * Budget owners can start funding and will see the fund button on project pages.
+             */
             $scope.makeBudgetOwner = function(index) {
                 User.makeBudgetOwner($scope.users[index]._id)
                     .success(function(user) {
                         $scope.users[index].is_budget_owner = user.is_budget_owner;
-
                     });
             }
-
         }
-    ]);
+    ]); // UsersViewCtrl END.
 
 })();
