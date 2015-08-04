adc25c4988c3d00bea9975a2be599750767c121e
Alex Tang <tang.alex.93@gmail.com>
2015-08-03 18:50:47 -0700

    Comment for ProjectVIew

--- public/ProjectView/projectCtrl.js
+++ public/ProjectView/projectCtrl.js
@@ -6,7 +6,16 @@
         "UserService",
         "ProjectService"
     ]);
-
+    
+    /**
+     * =========================================================
+     * Controller for the project view carousel modal.
+     * The modal is a carousel that takes in FilteredProjects and 
+     * allows the user to cycle through.
+     * 
+     * This page also allows users to comment, endorse, and fund.
+     * =========================================================
+     */
     app.controller("ProjectController", ["$scope", "$rootScope", "$location", "ModalService", "Project", "FilteredProjects", "Index", "IsPreview", "Image", "close",
         function($scope, $rootScope, $location, ModalService, Project, FilteredProjects, Index, IsPreview, Image, close) {
 
@@ -27,16 +36,24 @@
                 FilteredProjects[0].creator = $rootScope.user;
                 FilteredProjects[0].starImage = "/assets/icons/card-icons/Star.svg";
                 FilteredProjects[0].viewStarImage = "/assets/icons/card-icons/Star.svg";
-                FilteredProjects[0].image = URL.createObjectURL(Image);
+                FilteredProjects[0].image = Image ? URL.createObjectURL(Image) : "/assets/project_images/default_project_image.jpg"; 
+                // Genereate a temporary URL from the uploaded image.
             }
 
-            $scope.filteredProjects = FilteredProjects;
-            $scope.message;
-            $scope.currentIndex = Index;
-            $scope.slideIndex = Index;
-            $scope.fundAmount;
+            $scope.filteredProjects = FilteredProjects;     
+            $scope.currentIndex = Index;                // Number, Current Index of the Carousel (project currently being viewed)
+            $scope.slideIndex = Index;                  // Number, Index of the slide                          
 
-
+            $scope.showSection = {                      // The sections below the separation bar which get swapped out
+                showDetails: true,                      // set details as the default to be shown.
+                showBackers: false,
+                showComments: false,
+            }
+            
+            /**
+             * Display a modal for funding the currently viewed project.
+             * Triggered when the fundit button is pressed.
+             */
             $scope.fund = function() {
                 ModalService.showModal({
                     templateUrl: 'ProjectView/modals/FundModal.html',
@@ -48,6 +65,7 @@
                     modal.element.modal({
                         backdrop: 'static'
                     });
+                    // Set the z-indices for the black backgrounds.
                     $('.project-modal').css("z-index", 1050);
                     $('.modal-backdrop').css("z-index", 1060);
                     modal.close.then(function(result) {
@@ -56,19 +74,11 @@
                     });
                 });
             };
-
-            $scope.fundProject = function(project, index, fundAmount) {
-
-                Project.addBacker(project._id, $rootScope.user._id, fundAmount).
-                success(function(data) {
-                        $scope.filteredProjects[index].funded = data.funded;
-                        $scope.filteredProjects[index].backers = data.backers;
-                    })
-                    .error(function(data) {
-                        $scope.message = data;
-                    });
-            };
-
+            
+            /**
+             * Add a comment to the current project.
+             * Triggered when the add comment button is clicked.
+             */
             $scope.addComment = function() {
                 swal({
                         title: "Add a Comment",
@@ -91,14 +101,11 @@
                                 swal("Nice!", "You just added a comment!", "success");
                             });
                     });
-            }
-
-            $scope.showSection = {
-                showDetails: true,
-                showBackers: false,
-                showComments: false,
-            }
-
+            };
+            
+            /**
+             * Hide everything below the separation bar.
+             */
             $scope.hideAllSections = function() {
                 for (var key in $scope.showSection) {
                     if ($scope.showSection.hasOwnProperty(key)) {
@@ -106,38 +113,67 @@
                     }
                 }
             }
-
-            $scope.showBackers = function(project) {
-                $scope.hideAllSections();
-                $scope.showSection["showBackers"] = true;
-            }
-
+            
+            /**
+             * Show the details section
+             */
             $scope.showDetails = function(project) {
                 $scope.hideAllSections();
                 $scope.showSection["showDetails"] = true;
-            }
-
+            };
+            
+            /**
+             * Show the backers section
+             */
+            $scope.showBackers = function(project) {
+                $scope.hideAllSections();
+                $scope.showSection["showBackers"] = true;
+            };
+            
+            /**
+             * Show the comments section
+             */
             $scope.showComments = function(project) {
                 $scope.hideAllSections();
                 $scope.showSection["showComments"] = true;
-            }
+            };
 
+            /**
+             * Slide the carousel one slide left.
+             */
             $scope.slideLeft = function() {
                 $scope.slideIndex === 0 ? $scope.filteredProjects.length - 1 : $scope.slideIndex--;
-            }
+            };
+            
+            /**
+             * Slide the carousel one slide right.
+             */
             $scope.slideRight = function() {
                 $scope.slideIndex === $scope.filteredProjects.length - 1 ? 0 : $scope.slideIndex++;
-            }
-
+            };
+            
+            /**
+             * Close the modal and redirect to a different page.
+             * Parameter
+             *      url - the location to redirect to.
+             */
             $scope.closeModalAndRedirect = function(url) {
                 $scope.dismissModal();
                 $location.url(url);
             }
 
+            /**
+             * Close the projectView modal.
+             */
             $scope.dismissModal = function(result) {
                 close(result);
             };
 
+            /**
+             * Call to the server to follow a project.
+             * Parameter
+             *      projectId - the _id of the project.
+             */
             $scope.followProject = function(projectId) {
                 var currentProject = $scope.filteredProjects[$scope.currentIndex];
                 if (currentProject.isFollowed) {
@@ -152,62 +188,92 @@
                         });
                 }
             }
-
-
         }
-    ]);
+    ]); // ProjectController END.
 
-    app.controller("FundCtrl", ["$scope", "$rootScope", "Project", "ProjectToFund", "close", function($scope, $rootScope, Project, ProjectToFund, close) {
+    /**
+     * ====================================================
+     * Controller for FundModal.html.
+     * 
+     * This modal allows the user to fund a specific 
+     * project indicated by ProjectToFund.
+     * 
+     * The user must type in their own correct employee ID
+     * in order to fund the project.
+     * 
+     * This ID is not the _id in the database, it is 
+     * assigned by HSBC. 
+     * ====================================================
+     */
+    app.controller("FundCtrl", ["$scope", "$rootScope", "Project", "ProjectToFund", "close", 
+        function($scope, $rootScope, Project, ProjectToFund, close) {
 
-
-        $scope.project = ProjectToFund;
-        $scope.message;
-        $scope.accepted = "false";
-        $scope.fundProject = function(project, fundAmount) {
-            // Ensure the user has accepted the terms
-            if ($scope.accepted === "false") {
-                $scope.message = "Please accept the terms.";
-                return;
+            $scope.project = ProjectToFund;
+            $scope.message;
+            $scope.accepted = "false";
+            
+            /**
+             * Triggered when the user presses the FUNDIT button.
+             * It will update the message at the bottom depending on
+             * the inputs on the form. 
+             */
+            $scope.fundProject = function(project, fundAmount) {
+                // Ensure the user has accepted the terms
+                if ($scope.accepted === "false") {
+                    $scope.message = "Please accept the terms.";
+                    return;
+                }
+                // Ensure the user has entered the correct employee id
+                if ($scope.employee_id === $rootScope.user.employee_id) {
+                    $scope.message = "Your employee id is incorrect.";
+                    return;
+                }
+                // Ensure the user has funded a value greater than 0
+                if ($scope.fund_amount <= 0 || typeof $scope.fund_amount === "undefined") {
+                    $scope.message = "The fund amount is invalid";
+                    return;
+                }
+    
+                // Fund the project
+                $scope.funded = true;
+                $scope.fund_amount = "";
+                $scope.message = "Thanks for funding this project!"
+                Project.addBacker(project._id, $rootScope.user._id, fundAmount).
+                success(function(data) {
+                        ProjectToFund.funded = data.funded;
+                        ProjectToFund.backers = data.backers;
+                    })
+                    .error(function(data) {
+                        $scope.message = data;
+                    });
+            };
+            
+            /**
+             * This method is called when the 'I Accept' 
+             * check-box is pressed to change the color of the
+             * checkmark.
+             */
+            $scope.accept = function() {
+                if ($scope.accepted === "false") {
+                    $scope.accepted = "true";
+                } else {
+                    $scope.accepted = "false";
+                }
             }
-            // Ensure the user has entered the correct employee id
-            if ($scope.employee_id === $rootScope.user.employee_id) {
-                $scope.message = "Your employee id is incorrect.";
-                return;
-            }
-            // Ensure the user has funded a value greater than 0
-            if ($scope.fund_amount <= 0 || typeof $scope.fund_amount === "undefined") {
-                $scope.message = "The fund amount is invalid";
-                return;
-            }
-
-            // Fund the project
-            $scope.funded = true;
-            $scope.fund_amount = "";
-            $scope.message = "Thanks for funding this project!"
-            Project.addBacker(project._id, $rootScope.user._id, fundAmount).
-            success(function(data) {
-                    ProjectToFund.funded = data.funded;
-                    ProjectToFund.backers = data.backers;
-                })
-                .error(function(data) {
-                    $scope.message = data;
-                });
-        };
-
-        $scope.accept = function() {
-            if ($scope.accepted === "false") {
-                $scope.accepted = "true";
-            } else {
-                $scope.accepted = "false";
-            }
-        }
-
-        $scope.dismissModal = function(result) {
-            close(result);
-        };
+        
+            /**
+             * Close the modal.
+             */
+            $scope.dismissModal = function(result) {
+                close(result);
+            };
 
     }]);
-
+    
+    /**
+     * Filter for the resource link on the project page.
+     * It will allow the user to open a new tab.
+     */
     app.filter("link", function() {
         return function(link) {
             var result;
